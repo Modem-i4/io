e380e09dd27d12b414c6e92e006a67ef18aa8985
IhorZubenko <admin@oa.edu.ua>
2020-10-13 11:13:52 +0300

    add delete icon

--- resources/js/components/InventoryDepartmentsComponent.vue
+++ resources/js/components/InventoryDepartmentsComponent.vue
@@ -138,8 +138,26 @@
                                     ></v-combobox>
                                 </template>
                             </v-edit-dialog>
+                            <v-dialog v-model="dialogDelete" max-width="500px">
+                                <v-card>
+                                    <v-card-title class="headline">Are you sure you want to delete this item?</v-card-title>
+                                    <v-card-actions>
+                                        <v-spacer></v-spacer>
+                                        <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+                                        <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+                                        <v-spacer></v-spacer>
+                                    </v-card-actions>
+                                </v-card>
+                            </v-dialog>
                         </template>
-
+                        <template v-slot:item.actions="{ item }">
+                            <v-icon
+                                small
+                                @click="deleteItem(item)"
+                            >
+                                mdi-delete
+                            </v-icon>
+                        </template>
                     </v-data-table>
                     <v-snackbar
                         v-model="snack.visible"
@@ -178,6 +196,14 @@ export default {
     mixins: [DataTableCore],
     data () {
         return {
+            //////////////////
+            dialogDelete: false,
+            watch: {
+                dialogDelete (val) {
+                    val || this.closeDelete()
+                },
+            },
+            //////////////////////
             allDepartments: [],
             crudApiEndpoint: '/api/departments',
             headers: [
@@ -186,10 +212,30 @@ export default {
                 },
                 { text: "Назва", value: 'title' },
                 { text: 'Корпус', value: 'parent_department.title', fieldNameForSort: 'parent.title' },
+                { text: 'Дії', value: 'actions', sortable: false },
             ],
         }
     },
     methods: {
+        /////////////////////
+        deleteItem (item) {
+           // this.editedIndex = this.desserts.indexOf(item)
+            //this.editedItem = Object.assign({}, item)
+            this.dialogDelete = true
+        },
+
+        deleteItemConfirm () {
+          //  this.desserts.splice(this.editedIndex, 1)
+            this.closeDelete()
+        },
+        closeDelete () {
+            this.dialogDelete = false
+            this.$nextTick(() => {
+                this.editedItem = Object.assign({}, this.defaultItem)
+                this.editedIndex = -1
+            })
+        },
+        //////////////////
         prepareItemForUpdate(item) {
             return {
                 id: item.id,
