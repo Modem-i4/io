ed63e2e9877f908d09aa2deceaf4a5d2585d2363
IhorZubenko <admin@oa.edu.ua>
2020-10-20 00:45:59 +0300

    change navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,51 +21,86 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container-fluid">
-                @can('isAdmin')
-                        <a class="navbar-brand" href="{{ url('/admin/home') }}">
-                    @elsecan('isManager')
-                        <a class="navbar-brand" href="{{ url('/manager/home') }}">
-                    @else
-                        <a class="navbar-brand" href="{{ url('/user/home') }}">
-                @endcan
-                        <img class="mb-1 img-fluid" src="/img/oa_logo.png" alt="Інвентаризація Острозька академія" width="50px"> Інвентаризація ОА
-                        {{-- config('app.name','Laravel') --}}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
+                    <div class="logo mr-3">
+                            <a class="navbar-brand" href="
+                        @can('isAdmin') {{ url('/admin/home') }}
+                        @elsecan('isManager') {{ url('/manager/home') }}
+                        @else {{ url('/user/home') }}
+                        @endcan ">
+                                <img class="mb-1 img-fluid" src="/img/oa_logo.png" alt="Інвентаризація Острозька академія" width="50px"> Інвентаризація ОА{{-- config('app.name','Laravel') --}}
+                            </a>
+                    </div>
+                    <div class="collapse navbar-collapse align-items-center" id="navbarSupportedContent">
+                        <!-- Left Side Of Navbar -->
                         <ul class="navbar-nav mr-auto list-unstyled">
                             @can('isAdmin')
-                            <li class="nav-item mt-2">
-                              <a href="#" class="text-dark ml-2 d-flex align-items-center">
-                                  <svg class="icon icon-display mx-2"><use xlink:href="/img/icons/symbol-defs.svg#icon-display"></use></svg>Технічне обладнання
-                              </a>
-                            </li>
-                            <li class="nav-item mt-2">
-                              <a href="#" class="ml-2 {{ request()->routeIs('admin.departments.i')  ? 'active' : 'text-dark' }} d-flex align-items-center">
-                                  <svg class="icon icon-file-text2 mx-2"><use xlink:href="/img/icons/symbol-defs.svg#icon-file-text2"></use></svg>Накладні
-                              </a>
-                            </li>
+                                <li class="nav-item">
+                                    <a href="#" class="text-dark ml-2 d-flex align-items-center">
+                                        <svg class="icon icon-display mx-2" fill="currentColor"><use xlink:href="/img/icons/symbol-defs.svg#icon-display"></use></svg>Технічне обладнання
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="#" class="ml-2 {{ request()->routeIs('admin.departments.i')  ? 'active' : 'text-dark' }} d-flex align-items-center">
+                                        <svg class="icon icon-file-text2 mx-2" fill="currentColor"><use xlink:href="/img/icons/symbol-defs.svg#icon-file-text2"></use></svg>Накладні
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="#" class="text-dark ml-2 d-flex align-items-center">
+                                        <svg class="icon icon-transfer mx-2" fill="currentColor"><use xlink:href="/img/icons/symbol-defs.svg#icon-transfer"></use></svg>Передачі
+                                    </a>
+                                </li>
+                            @endcan
+                        </ul>
+                    </div>
+                    <!-- Right Side Of Navbar -->
+                    <div class="d-flex ml-auto align-items-center">
+                    <!--button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button-->
+                        <button
+                            class="navbar-toggler border-0 bg-transparent d-lg-none"
+                            type="button"
+                            data-toggle="collapse"
+                            data-target="#navbarSupportedContent"
+                            aria-controls="navbarSupportedContent"
+                            aria-expanded="false"
+                            aria-label="{{ __('Toggle navigation') }}"
+                        >
+                            <svg
+                                width="20px"
+                                height="20px"
+                                viewBox="0 0 16 16"
+                                class="bi bi-list"
+                                fill="currentColor"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    fill-rule="evenodd"
+                                    d="M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+                                />
+                            </svg>
+                        </button>
 
-                            <li class="nav-item mt-2">
-                              <a href="#" class="text-dark ml-2 d-flex align-items-center">
-                                  <svg class="icon icon-transfer mx-2"><use xlink:href="/img/icons/symbol-defs.svg#icon-transfer"></use></svg>Передачі
-                              </a>
-                            </li>
-                            <li class="mt-0 pl-1 nav-item dropdown">
-                                <a class="nav-link dropdown-toggle ml-2 d-flex align-items-center" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <svg class="icon icon-cog mx-2"><use xlink:href="/img/icons/symbol-defs.svg#icon-cog"></use></svg>Налаштування
-                              </a>
-                              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    @can('isAdmin')
+                        <div class="dropdown pr-1">
+                            <a
+                                class="dropdown-toggle gears text-decoration-none pr-1"
+                                href="#"
+                                role="button"
+                                id="dropdownMenuLink"
+                                data-toggle="dropdown"
+                                aria-haspopup="true"
+                                aria-expanded="false"
+                            >
+                                <svg class="icon icon-cog" fill="currentColor"><use xlink:href="/img/icons/symbol-defs.svg#icon-cog"></use></svg>
+                            </a>
+                            <div class="dropdown-menu gears dropdown-menu-right">
                                 <a href="{{ route('admin.departments.index') }}" class="dropdown-item d-flex align-items-center {{ request()->routeIs('admin.departments.index')  ? 'active' : 'text-dark' }}">
                                     <svg class="icon icon-home mx-2"><use xlink:href="/img/icons/symbol-defs.svg#icon-home"></use></svg>Приміщення
                                 </a>
                                 <a href="#" class="dropdown-item text-dark d-flex align-items-center">
                                     <svg class="icon icon-list mx-2"><use xlink:href="/img/icons/symbol-defs.svg#icon-list"></use></svg>Типи обладнання
-                              </a>
+                                </a>
 
                                 <div class="dropdown-divider"></div>
                                 <a href="{{ route('admin.users.index') }}" class="dropdown-item text-dark d-flex align-items-center {{ request()->routeIs('admin.users.index')  ? 'active' : 'text-dark' }}">
@@ -74,60 +109,67 @@
                                 <a href="#" class="dropdown-item text-dark d-flex align-items-center">
                                     <svg class="icon icon-users mx-2"><use xlink:href="/img/icons/symbol-defs.svg#icon-users"></use></svg>Ролі користувачів
                                 </a>
-                              </div>
-                            </li>
+                            </div>
+                        </div>
 
+                            <a href=""
+                            type="button"
+                            class="border-0 bg-transparent pr-3"
+                            data-toggle="tooltip"
+                            data-placement="right"
+                            title="{{ __('Вийти') }}"
+                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
 
-
-                            @endcan
-                          </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdownLogin" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                    @can('isAdmin')
-                                        <span class="badge badge-success">
-                                        Адміністратор
-                                        </span>
-                                    @elsecan('isManager')
-                                        <span class="badge badge-primary">
-                                        Менеджер
-                                        </span>
-                                    @else
-                                        <span class="badge badge-info">
-                                        Користувач
-                                        </span>
-                                    @endcan
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownLogin">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Вийти') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
+                        >
+                            <svg
+                                width="1em"
+                                height="1em"
+                                viewBox="0 0 16 16"
+                                class="bi bi-box-arrow-in-right"
+                                fill="currentColor"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    fill-rule="evenodd"
+                                    d="M8.146 11.354a.5.5 0 0 1 0-.708L10.793 8 8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0z"
+                                />
+                                <path
+                                    fill-rule="evenodd"
+                                    d="M1 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 1 8z"
+                                />
+                                <path
+                                    fill-rule="evenodd"
+                                    d="M13.5 14.5A1.5 1.5 0 0 0 15 13V3a1.5 1.5 0 0 0-1.5-1.5h-8A1.5 1.5 0 0 0 4 3v1.5a.5.5 0 0 0 1 0V3a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v10a.5.5 0 0 1-.5.5h-8A.5.5 0 0 1 5 13v-1.5a.5.5 0 0 0-1 0V13a1.5 1.5 0 0 0 1.5 1.5h8z"
+                                />
+                            </svg>
+                        </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                            <img src="{{ Auth::user()->avatar }}" alt="avatar" class="img-fluid rounded" width="45px">
+                            <div class="d-none align-items-center px-2 d-sm-flex">
+                                <div>
+                                    <span>{{ Auth::user()->name }}</span>
+                                    <span class="d-block">@can('isAdmin')
+                                            <span class="badge badge-success">
+                                            Адміністратор
+                                            </span>
+                                        @elsecan('isManager')
+                                            <span class="badge badge-primary">
+                                            Менеджер
+                                            </span>
+                                        @else
+                                            <span class="badge badge-info">
+                                            Користувач
+                                            </span>
+                                            @endcan
+                                    </span>
                                 </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
+                            </div>
+
+
+                    @endcan
+                    </div>
             </div>
         </nav>
 
