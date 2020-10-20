4054c25b352a1d72e6a2d504eb6aef6e12afc4ef
IhorZubenko <admin@oa.edu.ua>
2020-10-20 11:10:33 +0300

    change icons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -66,19 +66,7 @@
                             aria-expanded="false"
                             aria-label="{{ __('Toggle navigation') }}"
                         >
-                            <svg
-                                width="20px"
-                                height="20px"
-                                viewBox="0 0 16 16"
-                                class="bi bi-list"
-                                fill="currentColor"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path
-                                    fill-rule="evenodd"
-                                    d="M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
-                                />
-                            </svg>
+                            <svg class="icon icon-cog" fill="currentColor"><use xlink:href="/img/icons/symbol-defs.svg#icon-menu"></use></svg>
                         </button>
 
                     @can('isAdmin')
@@ -119,35 +107,14 @@
                             data-placement="right"
                             title="{{ __('Вийти') }}"
                             onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
-
-                        >
-                            <svg
-                                width="1em"
-                                height="1em"
-                                viewBox="0 0 16 16"
-                                class="bi bi-box-arrow-in-right"
-                                fill="currentColor"
-                                xmlns="http://www.w3.org/2000/svg"
                             >
-                                <path
-                                    fill-rule="evenodd"
-                                    d="M8.146 11.354a.5.5 0 0 1 0-.708L10.793 8 8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0z"
-                                />
-                                <path
-                                    fill-rule="evenodd"
-                                    d="M1 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 1 8z"
-                                />
-                                <path
-                                    fill-rule="evenodd"
-                                    d="M13.5 14.5A1.5 1.5 0 0 0 15 13V3a1.5 1.5 0 0 0-1.5-1.5h-8A1.5 1.5 0 0 0 4 3v1.5a.5.5 0 0 0 1 0V3a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v10a.5.5 0 0 1-.5.5h-8A.5.5 0 0 1 5 13v-1.5a.5.5 0 0 0-1 0V13a1.5 1.5 0 0 0 1.5 1.5h8z"
-                                />
-                            </svg>
+                                <svg class="icon icon-cog" fill="currentColor"><use xlink:href="/img/icons/symbol-defs.svg#icon-exit"></use></svg>
                         </a>
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                 @csrf
                             </form>
-                            <img src="{{ Auth::user()->avatar }}" alt="avatar" class="img-fluid rounded" width="45px">
-                            <div class="d-none align-items-center px-2 d-sm-flex">
+                            <img src="{{ Auth::user()->avatar }}" alt="avatar" class="img-fluid rounded" width="45px" title="{{ Auth::user()->name }}">
+                            <div class="d-none align-items-center px-2 d-md-flex">
                                 <div>
                                     <span>{{ Auth::user()->name }}</span>
                                     <span class="d-block">@can('isAdmin')
