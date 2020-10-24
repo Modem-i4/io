fe3a48e3d6f4f955afb2c994994c50708e3bc5ab
mlutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2020-10-24 18:38:02 +0300

    Created missed controllers, cleaned up ItemController

---
+++ app/Http/Controllers/Inventory/Admin/TypeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Inventory\Admin;
+
+class TypeController extends BaseController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        dd(__method__);
+    }
+}
