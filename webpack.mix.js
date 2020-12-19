9b723aec5c10a624817d78b4a558c13706b1e370
mlutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2020-12-19 18:53:22 +0200

    Added laravel-mix-bundle-analyzer package. Run npm run prod to get stats.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,12 @@
 const mix = require('laravel-mix');
 
+require('laravel-mix-bundle-analyzer');
 require('vuetifyjs-mix-extension');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .vuetify('vuetify-loader');
+
+if (mix.inProduction()) {
+    mix.bundleAnalyzer();
+}
